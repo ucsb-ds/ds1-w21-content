32c06fb15f7eda6f3edff6401b13cfc610e55fbf
sharon <swsolis@ucsb.edu>
2021-01-20 23:34:21 +0000

    removed old lab03

--- labs/lab03/tests/q2_1.py
+++
@@ -1,9 +0,0 @@
-test = {   'name': 'q2_1',
-    'points': 2,
-    'suites': [   {   'cases': [   {'code': '>>> top_10_movies.num_rows == 10\nTrue', 'hidden': False, 'locked': False},
-                                   {'code': '>>> top_10_movies.num_columns == 2\nTrue', 'hidden': False, 'locked': False},
-                                   {'code': ">>> top_10_movies.labels == ('Rating', 'Name')\nTrue", 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
