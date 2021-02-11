8eb3293e54d26ad963a005b4fa9ddbf1b36e3fb1
sharon <swsolis@ucsb.edu>
2021-02-11 00:57:50 +0000

    added hw03

---
+++ hw/hw03/tests/q1_1.py
@@ -0,0 +1,13 @@
+test = {   'name': 'q1_1',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> # Make sure you have all the columns from both tables;\n'
+                                               '>>> set(["Name", "Menu_Item", "Yelp", \n'
+                                               '...      "Google", "Overall", "Cost"]) == set(burritos.labels)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {'code': '>>> burritos.num_rows == 212\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
