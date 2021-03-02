30fb519406048474ec2f4df22b9062d8654c0670
sharon <swsolis@ucsb.edu>
2021-03-02 00:17:57 +0000

    added final-project notebook and rubric and writeup

---
+++ final-project/tests/q1_12_b.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1_12_b',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': ">>> pop_by_decade.labels == ('decade', 'population')\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': '>>> pop_by_decade.column(0).item(0) == 1960\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
